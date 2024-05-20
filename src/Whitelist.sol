35e01f73c65c63e210bc7183c04066b425ba02b9
Luca Pulina <luca.pulina@gmail.com>
2024-05-20 16:34:34 +0200

    feat: create contract

---
+++ src/Whitelist.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Whitelist {
+    error Whitelist__SenderHasAlreadyBeenWhitelisted();
+    error Whitelist__AddressListIsFull();
+
+    uint8 public immutable i_maxWhitelistedAddresses; // list of whitelisted addresses allowed
+    mapping(address => bool) public whitelistedAddresses;
+    uint8 public s_numAddressesWhitelisted; // how many addresses are whitelisted
+
+    constructor(uint8 _maxWhitelistedAddresses) {
+        i_maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    function addAddressToWhitelist() public {
+        if (whitelistedAddresses[msg.sender] == true) {
+            revert Whitelist__SenderHasAlreadyBeenWhitelisted();
+        }
+
+        if (s_numAddressesWhitelisted >= i_maxWhitelistedAddresses) {
+            revert Whitelist__AddressListIsFull();
+        }
+
+        whitelistedAddresses[msg.sender] = true;
+        s_numAddressesWhitelisted++;
+    }
+}
