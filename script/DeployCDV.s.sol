c9dd7fa01fd9c32784574cc5d1ef55072c4f2df7
Luca Pulina <luca.pulina@gmail.com>
2024-05-21 11:44:09 +0200

    feat: udpate deploy script

--- script/DeployCDV.s.sol
+++ script/DeployCDV.s.sol
@@ -3,14 +3,16 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {Whitelist} from "../src/Whitelist.sol";
+import {CryptoDevs} from "../src/CryptoDevs.sol";
 
-contract DeployWhitelist is Script {
+contract DeployCDV is Script {
     uint8 constant MAX_WITHELIST_ADDRESSES = 2;
 
-    function run() external returns (Whitelist) {
+    function run() external returns (Whitelist, CryptoDevs) {
         vm.startBroadcast();
         Whitelist _whitelist = new Whitelist(MAX_WITHELIST_ADDRESSES);
+        CryptoDevs _cryptoDevs = new CryptoDevs(address(_whitelist));
         vm.stopBroadcast();
-        return _whitelist;
+        return (_whitelist, _cryptoDevs);
     }
 }
