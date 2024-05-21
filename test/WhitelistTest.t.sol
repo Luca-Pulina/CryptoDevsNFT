3db15cbdaf0b1e4bb3a47ee793abbc4ad3868977
Luca Pulina <luca.pulina@gmail.com>
2024-05-21 11:44:53 +0200

    feat: adapt WthitelistTest to new deployer

--- test/WhitelistTest.t.sol
+++ test/WhitelistTest.t.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {Whitelist} from "../src/Whitelist.sol";
-import {DeployWhitelist} from "../script/DeployWithelist.s.sol";
+import {DeployCDV} from "../script/DeployCDV.s.sol";
 
 contract WhitelistTest is Test {
     Whitelist public whitelist;
+
     address public PLAYER_1 = makeAddr("player1");
     address public PLAYER_2 = makeAddr("player2");
     address public PLAYER_3 = makeAddr("player3");
 
     function setUp() public {
-        DeployWhitelist deployWhitelist = new DeployWhitelist();
-        whitelist = deployWhitelist.run();
+        DeployCDV deployCDV = new DeployCDV();
+        (whitelist,) = deployCDV.run();
     }
 
     function testOnlyOnceAddress() public {
